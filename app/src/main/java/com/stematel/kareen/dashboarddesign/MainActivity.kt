70cef9366b36955a054144cffcfa1308418a5bdf
kareenawidagdo <widagdo.kareena@gmail.com>
2021-03-31 14:03:02 +0700

    first commit

---
+++ app/src/main/java/com/stematel/kareen/dashboarddesign/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.stematel.kareen.dashboarddesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = resources.getColor(R.color.item)
+    }
+}
